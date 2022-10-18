9c1f7c8ee60bd9135cfddae1a27fbb501d00c090
Micaela Perez <micaelaperez663@gmail.com>
2022-10-17 21:46:11 -0300

    initial commit

---
+++ src/components/Formulario/Detalle.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useContext } from "react";
+import { InputContext } from "../../context/ContextoFormulario";
+
+const Detalle = () => {
+  // Aqui deberíamos obtener los datos del formulario para poder mostrarlo en
+  // la vista previa.
+  const [input, setInput] = useContext(InputContext);
+
+  return (
+    <div className="detalle-formulario">
+      <div className="encabezado">
+        <h3>Vista Previa de la Solicitud</h3>
+      </div>
+      <section className="datos-cliente">
+        <h4>Datos del Entrenador</h4>
+        <div className="fila">
+          <p>Nombre:{input}</p>
+          <p>Apellido:</p>
+          <p>Email:</p>
+        </div>
+      </section>
+      <section className="datos-cliente">
+        <h4>Datos del Pokémon</h4>
+        <div className="fila">
+          <p>Nombre:</p>
+        </div>
+      </section>
+      <button
+        className="boton-enviar"
+        onClick={() => alert("Solicitud enviada :)")}
+      >
+        Enviar Solicitud
+      </button>
+    </div>
+  );
+};
+
+export default Detalle;
